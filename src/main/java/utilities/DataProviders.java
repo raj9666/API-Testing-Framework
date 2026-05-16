da279d960dcf8704704c660b4fabc5881560babe
manjula <nagarammanjula02@gmail.com>
2026-05-16 18:26:20 +0530

    ss

--- src/main/java/utilities/DataProviders.java
+++ src/main/java/utilities/DataProviders.java
@@ -33,7 +33,7 @@ public class DataProviders {
 	@DataProvider(name="Names")
 	public String[] getUserNames() throws IOException
 	{
-	
+	//
 		int rownum=xl.getRowCount("Sheet1");	
 			
 		String testdata[]=new String[rownum];
